81a0bade7abbf03c42ab000d97aa078d76a4eabf
bmo-crayola <uzamats@gmail.com>
2025-11-09 17:19:56 -0800

    Added UserDAO implementation

--- app/src/main/java/database/UserDAO.java
+++ app/src/main/java/database/UserDAO.java
@@ -1,4 +1,33 @@
 package database;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+import database.entities.User;
+
+@Dao
 public interface UserDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(User... user);
+
+    @Delete
+    void delete(User user);
+
+    @Query("SELECT * FROM " + "userTable" + " ORDER BY username")
+    LiveData<List<User>> getAllUsers();
+
+    @Query("DELETE from " + "userTable")
+    void deleteAll();
+
+    @Query("SELECT * from " + "userTable" + " WHERE username == :username")
+    LiveData<User> getUserByUserName(String username);
+
+    @Query("SELECT * from " + "userTable" + " WHERE id == :userId")
+    LiveData<User> getUserByUserId(int userId);
 }
