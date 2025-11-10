87dba11d3cd9a8cce061ce1800566ea74845dcb5
bmo-crayola <uzamats@gmail.com>
2025-11-09 17:05:12 -0800

    Added RoomDB into build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    val room_version = "2.8.3"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 }
