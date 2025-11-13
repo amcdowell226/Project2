48d4a69c825e16338e42f9cf9c2ec1911f29797f
warauko <tisanijohnson@gmail.com>
2025-11-12 16:41:55 -0800

    To dos

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,25 +1,29 @@
 package com.example.project2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
 
+    // TODO: Detect if user has already logged in and set to true if so, skips this entire activity using sharedPreference
+    // Context context = getActivity();
+    //SharedPreferences sharedPref = context.getSharedPreferences(
+    //getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+    boolean loggedIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button loginPrompt = findViewById(R.id.loginButton);
+
     }
+// TODO: OnClickListener
 }
