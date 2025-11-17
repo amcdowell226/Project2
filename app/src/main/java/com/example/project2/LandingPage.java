1ef58d2b945437bdd17556aaf707c641fcebc869
bmo-crayola <uzamats@gmail.com>
2025-11-16 19:32:40 -0800

    Commited some work to LandingPage branch

--- app/src/main/java/com/example/project2/LandingPage.java
+++ app/src/main/java/com/example/project2/LandingPage.java
@@ -1,18 +1,26 @@
 package com.example.project2;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
 
 import com.example.project2.databinding.ActivityLandingPageBinding;
 
 import database.ProduceLogRepository;
+import database.entities.User;
 
 public class LandingPage extends AppCompatActivity {
     private static final String MAIN_ACTIVITY_USER_ID = "com.example.project2.MAIN_ACTIVITY_USER_ID";
@@ -22,11 +30,113 @@ public class LandingPage extends AppCompatActivity {
     private ActivityLandingPageBinding binding;
     private ProduceLogRepository repository;
 
+    private int loggedInUserId = -1;
+    private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
 
+        binding = ActivityLandingPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = ProduceLogRepository.getRepository(getApplication());
+        loginUser(savedInstanceState);
+
+
+        // Seemingly we don't need the chunk below.
+
+        //User is not logged in at this point, go to login screen
+//        if (loggedInUserId == -1) {
+//            Intent intent = MainActivity.loginIntentFactory(getApplicationContext());
+//            startActivity(intent);
+//        }
+
+        updateSharedPreference();
+    }
+
+    private void loginUser(Bundle savedInstanceState) {
+        //check shared preference for logged in user read from the file
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("PreferenceFileNameProduceLog",
+                Context.MODE_PRIVATE);
+
+        loggedInUserId = sharedPreferences.getInt("PreferenceUserIdKeyProduceLog", LOGGED_OUT);
+
+        if (loggedInUserId == LOGGED_OUT & savedInstanceState != null && savedInstanceState.containsKey(SAVED_INSTANCE_STATE_USERID_KEY)) {
+            loggedInUserId = savedInstanceState.getInt(SAVED_INSTANCE_STATE_USERID_KEY, LOGGED_OUT);
+        }
+        if (loggedInUserId == LOGGED_OUT) {
+            loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, LOGGED_OUT);
+        }
+        if (loggedInUserId == LOGGED_OUT) {
+            return;
+        }
+
+        LiveData<User> userObserver = repository.getUserByUserId(loggedInUserId);
+        userObserver.observe(this, user -> {
+            this.user = user;
+            if (user != null) {
+                invalidateOptionsMenu();
+            }
+        });
+    }
+
+    private void showLogoutDialog() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(LandingPage.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+
+        alertBuilder.setMessage("Logout?");
+
+        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertBuilder.create().show();
+    }
+
+    private void logout() {
+        loggedInUserId = LOGGED_OUT;
+        updateSharedPreference();
+
+        getIntent().putExtra(MAIN_ACTIVITY_USER_ID, LOGGED_OUT);
+
+        startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.buttonLogOut);
+        item.setVisible(true);
+        if (user == null) {
+            return false;
+        }
+        item.setTitle(user.getUsername());
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem menuItem) {
+                showLogoutDialog();
+                return false;
+            }
+        });
+        return true;
+    }
+
+    private void updateSharedPreference() {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("PreferenceFileNameProduceLog",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
+        sharedPrefEditor.putInt("PreferenceUserIdKeyProduceLog", loggedInUserId);
+        sharedPrefEditor.apply();
     }
 
     static Intent landingActivityIntentFactory(Context context, int userId) {
