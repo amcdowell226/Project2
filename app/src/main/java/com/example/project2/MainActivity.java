da4407f72e2d5db7708bc2af90602ccb6a9f1155
warauko <tisanijohnson@gmail.com>
2025-11-16 17:26:51 -0800

    Added onClick Listener for login, won't work until merged and can see login activity class

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.project2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,8 +24,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginPrompt = findViewById(R.id.loginButton);
 
+        Button loginPrompt = findViewById(R.id.loginButton);
+        loginPrompt.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }));
     }
-// TODO: OnClickListener
+
 }
