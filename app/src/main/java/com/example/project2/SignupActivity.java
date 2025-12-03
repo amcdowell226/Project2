8a60f9f36b5c78493a602ef1702816b8698065df
warauko <tisanijohnson@gmail.com>
2025-12-03 15:12:10 -0800

    Skeleton of signup page.

---
+++ app/src/main/java/com/example/project2/SignupActivity.java
@@ -0,0 +1,24 @@
+package com.example.project2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.project2.databinding.ActivitySignupBinding;
+
+
+public class SignupActivity extends AppCompatActivity {
+    private ActivitySignupBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
