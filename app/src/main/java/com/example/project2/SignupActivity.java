f546ebdb07a51f819e0bfdabe559bc378eef33f2
warauko <tisanijohnson@gmail.com>
2025-12-07 00:35:33 -0800

    This can't be set up properly until it's merged.

--- app/src/main/java/com/example/project2/SignupActivity.java
+++ app/src/main/java/com/example/project2/SignupActivity.java
@@ -1,18 +1,26 @@
 package com.example.project2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
 
 import com.example.project2.databinding.ActivitySignupBinding;
+
+import database.ProduceLogRepository;
+import database.entities.User;
 
 
 public class SignupActivity extends AppCompatActivity {
     private ActivitySignupBinding binding;
+    private ProduceLogRepository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,46 @@ public class SignupActivity extends AppCompatActivity {
         binding = ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        repository = ProduceLogRepository.getRepository(getApplication());
+
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+    }
+
+    private void setContentView(ConstraintLayout root) {
+    }
+
+    private void createUser() {
+        String username = String.valueOf(binding.usernameLayout);
+        if (username.isEmpty()) {
+            toastMaker("Username may not be blank.");
+            return;
+        }
+
+        String password = String.valueOf(binding.passwordLayout);
+        if (password.isEmpty()) {
+            toastMaker("Password may not be blank.");
+            return;
+        }
+
+        User newUser = new User(username, password);
+
+        LiveData<User> userObserver = repository.getUserByUserName(username);
+        userObserver.observe(this, user -> {
+            if (user != null) {
+                toastMaker("This user already exists.");
+            } else {
+                // TODO: Make this connect properly when merged.
+//                repository.insertUser(newUser);
+                toastMaker("New user has been added!");
+            }
+        });
+    }
+    private void toastMaker (String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
