3c94d80c067e5deeaa045707c6a09f30cfa95db0
Katie <katismith@csumb.edu>
2025-12-07 20:06:56 -0800

    got it to kinda work hehe

--- app/src/main/java/com/example/project2/EditProduceActivity.java
+++ app/src/main/java/com/example/project2/EditProduceActivity.java
@@ -1,21 +1,28 @@
 package com.example.project2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
 
 import com.example.project2.database.ProduceLogRepository;
+import com.example.project2.database.entities.Product;
+import com.example.project2.database.entities.User;
 import com.example.project2.databinding.ActivityEditProduceBinding;
 import com.example.project2.databinding.ActivityEditUsersBinding;
 
 public class EditProduceActivity extends AppCompatActivity {
     private ActivityEditProduceBinding binding;
     private ProduceLogRepository repository;
+    private int loggedInUserId;
 
 
     @Override
@@ -23,6 +30,7 @@ public class EditProduceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityEditProduceBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        loggedInUserId = getIntent().getIntExtra("loggedIn_UserId_EditProduceActivity", -1);
 
         repository = ProduceLogRepository.getRepository(getApplication());
 
@@ -52,10 +60,44 @@ public class EditProduceActivity extends AppCompatActivity {
         String pName = binding.nameEditText.getText().toString();
         String pType = binding.typeEditText.getText().toString();
 
-        if()
+        if(pName.isEmpty()){
+            toastMaker("Produce needs to be named");
+            return;
+        }
+        if(pType.isEmpty()){
+            toastMaker("Produce needs a type");
+            return;
+        }
+
+        Product p = new Product(pName, pType);
+        repository.insertProduct(p);
     }
 
     private void deleteProduce() {
+        String pName = binding.nameEditText.getText().toString();
+        if(pName.isEmpty()){
+            toastMaker("Produce needs to be named");
+            return;
+        }
 
+        LiveData<Product> productObserver = repository.getProduceByName(pName);
+        productObserver.observe(this, product -> {
+            if(product != null){
+                repository.deleteProduct(product);
+                toastMaker("Produce has been deleted!");
+            } else {
+                toastMaker("This produce does not exist.");
+                binding.nameEditText.setSelection(0);
+            }
+        });
+    }
+
+    static Intent editProduceIntentFactory(Context context, int userId) {
+        Intent intent = new Intent(context, EditProduceActivity.class);
+        intent.putExtra("loggedIn_UserId_EditProduceActivity", userId);
+        return intent;
+    }
+    private void toastMaker(String message) {
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
     }
 }
