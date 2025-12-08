f00551875e956dc3e9968f9842028a5fedbd0e6d
Aliyah <aliyahmcdowell777@gmail.com>
2025-12-07 16:21:27 -0800

    start of the test files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.core)
+    implementation(libs.ext.junit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/test/java/com/example/project2/DBUnitTest.java
@@ -0,0 +1,43 @@
+package com.example.project2;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.project2.database.ProduceLogDatabase;
+import com.example.project2.database.ProductDAO;
+import com.example.project2.database.RecipeLogDAO;
+import com.example.project2.database.UserDAO;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+@RunWith(AndroidJUnit4.class)
+public class DBUnitTest {
+
+    private UserDAO userDAO;
+    private ProductDAO productDAO;
+    private RecipeLogDAO recipeLogDAO;
+    private ProduceLogDatabase db;
+
+    @Before
+    public void createDb(){
+        Context context = ApplicationProvider.getApplicationContext();
+        db = Room.inMemoryDatabaseBuilder(context, ProduceLogDatabase.class).build();
+        userDAO = db.userDAO();
+        productDAO = db.productDAO();
+        recipeLogDAO = db.recipeLogDAO();
+    }
+    @After
+    public void closeDb() throws IOException {
+        db.close();
+    }
+}
